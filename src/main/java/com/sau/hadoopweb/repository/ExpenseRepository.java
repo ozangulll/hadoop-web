0ec9977e99ccd568e710e0c49f9d28929816cad7
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-12-11 00:37:18 +0300

    cannot reliably process 'remove' call" issue has been solved.

--- src/main/java/com/sau/hadoopweb/repository/ExpenseRepository.java
+++ src/main/java/com/sau/hadoopweb/repository/ExpenseRepository.java
@@ -2,9 +2,15 @@ package com.sau.hadoopweb.repository;
 
 import com.sau.hadoopweb.model.Employee;
 import com.sau.hadoopweb.model.Expense;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
     Expense findByEmployee(Employee employee);
+
+
+    @Transactional
+    void deleteByEmployeeId(int employee_id);
+
 }
